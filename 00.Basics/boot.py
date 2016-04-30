28f0503e00f73f1132ec4719238fdb1edb3aad48
Mithru-Chromebook <mithru27@gmail.com>
2016-04-30 19:25:10 +0800

    updated boot.py

--- 00.Basics/boot.py
+++ 00.Basics/boot.py
@@ -10,8 +10,62 @@ Replace 'helloWorld.py' with the name of your
 script.
 '''
 
+'''
+If you're using the MicroPython-Examples github repo,
+just uncomment the required line.
+'''
+
+#00.Basics/
+
 pyb.main('helloWorld.py') # main script to run after this one
 
+
+#01.LEDs/
+
+#pyb.main('blink.py') 
+#pyb.main('blinkToggle.py') 
+#pyb.main('blinkWithoutDelay.py') 
+#pyb.main('fade.py') 
+#pyb.main('heartbeat.py') 
+#pyb.main('heartbeatFade.py') 
+
+
+#02.Inputs/
+
+#pyb.main('button.py') 
+#pyb.main('potentiometer.py') 
+#pyb.main('switch.py') 
+#pyb.main('switchCallback.py') 
+
+
+#03.Pins/
+
+#pyb.main('PinsBasicOutput.py') 
+
+
+#04.Accelerometer/
+
+#pyb.main('accelerometer.py') 
+#pyb.main('accelerometerControlLED.py') 
+
+
+#05.Servos
+
+#pyb.main('ServoGetAngle.py') 
+#pyb.main('ServoSetAngle.py')
+
+
+#06.Clock
+
+#pyb.main('clock.py') 
+
+
+#07.Mouse
+# has it's own boot.py (this needs to be placed better)
+
+#08.Sensors
+
+#pyb.main('HC-SR04.py') 
 
 
 #pyb.usb_mode('CDC+MSC') # act as a serial and a storage device
